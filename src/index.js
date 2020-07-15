7f8fa721aafc10545bcef804cadb58a5361a4f66
Kori <korneliaszabo@kinsta.com>
2020-07-15 18:38:13 +0200

    :sparkles: add routing to our app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ErrorBoundary } from 'react-error-boundary';
+import { Route, Router, Switch } from 'react-router-dom';
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import client from "./apollo";
 import { ApolloProvider } from "@apollo/react-hooks";
+import history from './history';
+
+const Root = () => {
+  return (
+    <ErrorBoundary>
+        <Router history={history}>
+          <Switch>
+            <Route exact path={'/search'} component={App} />
+          </Switch>
+        </Router>
+    </ErrorBoundary>
+  );
+};
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />
   </ApolloProvider>,
   document.getElementById("root")
 );
